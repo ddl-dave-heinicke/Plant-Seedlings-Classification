80b5434a104334ea469dc5615bb1dacf75b7ee36
Dave <31716582+dheinicke1@users.noreply.github.com>
2018-09-15 15:38:03 -0600

    Ongoing EDA

--- Transit/files/transit_EDA.py
+++ Transit/files/transit_EDA.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 import gc
 import matplotlib.pyplot as plt
 from collections import defaultdict, OrderedDict
 
 from pandas.plotting import scatter_matrix
 import seaborn as sns
+plt.style.use('seaborn')
 
-# DATA_PATH = 'C:\\Users\\Dave\\Documents\\Python Scripts\\Transit\\'
+DATA_PATH = 'C:\\Users\\Dave\\Documents\\Python Scripts\\Transit\\'
 
-DATA_PATH = 'C:\\Users\\dheinicke\\Google Drive\\Data Science Training\\Python Scripts\\Transit\\'
+# DATA_PATH = 'C:\\Users\\dheinicke\\Google Drive\\Data Science Training\\Python Scripts\\Transit\\'
 
 # Definitions
 
@@ -178,8 +180,6 @@ EDA_1_by_agency.drop(EDA_1_by_agency.Service_Area_Pop_Density.
 # Investigate outliers
 EDA_1_by_agency.iloc[EDA_1_by_agency.Passenger_Trips_PC.idxmax()]
 
-EDA_1_by_agency.loc[EDA_1_by_agency.UZA_Name.str.contains('KY')]
-
 # Drop Population Outliers - only service areas between 50k and 1 million
 min_pop = 0000
 max_pop = 10000000
@@ -222,7 +222,7 @@ sns.heatmap(temp, annot=True)
 
 gc.collect()
 
-# Objective # 2 - Where is ridership increasing, and where is it decresing?
+# Objective # 2 - Is ridership increasing or decreasing?
 
 # 2.1 Check missing values
 
@@ -247,33 +247,210 @@ df_UPT = full_data[UPT_cols]
 
 df_UPT_by_type = df_UPT[UPT_cols[2:]].groupby('Modes').sum().T
 df_UPT_by_type = df_UPT_by_type.fillna(0)
-# df_UPT_by_type.head()
 plot_total_ridership(df_UPT_by_type, freq='quarterly')
-df_UPT_by_type.head()
+
 # Try interpolating missing values. Don't interpolate if more than 5 years
 # are missing to avoid over-estimating. Aligns with APTA estimates
-
-df_UPT_int = df_UPT[UPT_cols[3:]].dropna(thresh=60)
-df_UPT_int = df_UPT_int.interpolate(axis=1, limit=None, limit_direction='both')
+df_UPT = df_UPT.dropna(thresh=60)
+df_UPT_int = df_UPT[UPT_cols[3:]].interpolate(axis=1,
+                                              limit=None,
+                                              limit_direction='both')
 df_UPT_int['Modes'] = df_UPT['Modes']
-df_UPT_by_type = df_UPT_int.groupby('Modes').sum().T
+df_UPT_by_type = df_UPT_int.groupby('Modes').sum()
+plot_total_ridership(df_UPT_by_type.T, freq='yearly')
 
-plot_total_ridership(df_UPT_by_type, freq='quarterly')
-
-# 2.2 Break down total annual ridership by type
-
+# 2.2 Break down total annual ridership by mode
 df_UPT_by_type['Total'] = df_UPT_by_type.sum(axis=1)
 df_UPT_by_type = df_UPT_by_type.sort_values(by='Total', ascending=False)
 
-fig, ax = plt.subplots(figsize=(14, 14))
-ax = df_UPT_by_type.plot()
-ax.set_title('Total Ridership in the United States by Type',
-             fontsize=16)
-ax.set_ylabel('Total Trips per Year', fontsize=14)
+# Plot monthly ridership of 5 most common modes, without total column
+fig = df_UPT_by_type.T.iloc[:-1, 0:5].plot(figsize=(12, 12))
+fig.set_title('Total Ridership in the United States by Type',
+              fontsize=16)
+fig.set_ylabel('Total Trips per Year', fontsize=14)
 plt.show()
 
-df_UPT_by_type.head(22)
-df_UPT_by_type.shape
+# 2.3 Investigate relationship between changes in total annual ridership and
+# population served by agency
+
+# Aggregate by transit agency ID, Create population bin feature
+
+# Select columns, interpolate missing values
+UPT_cols = ['5_digit_NTD_ID',
+            'UZA_Area_SQ_Miles',
+            'UZA_Population',
+            'Service_Area_SQ_Miles',
+            'Service_Area_Population']
+
+for col in full_data.columns:
+    if col.endswith('UPT'):
+        UPT_cols.append(col)
+
+df_UPT = full_data[UPT_cols]
+
+df_UPT = df_UPT.dropna(thresh=60)
+df_UPT_int = df_UPT[UPT_cols[5:]].interpolate(axis=1,
+                                              limit=None,
+                                              limit_direction='both')
+df_UPT_int[UPT_cols] = df_UPT[UPT_cols]
+
+# Agency properties to be aggregated by mode, ridership columns by sum
+
+grouped = df_UPT_int.groupby('5_digit_NTD_ID')
+
+sum_cols = []
+
+for col in full_data.columns:
+    if col.endswith('UPT'):
+        sum_cols.append(col)
+
+agency_cols = ['UZA_Area_SQ_Miles',
+               'UZA_Population',
+               'Service_Area_SQ_Miles',
+               'Service_Area_Population']
+
+funcs = defaultdict()
+
+for col in df_UPT_int.columns:
+    if col in sum_cols:
+        funcs[col] = np.sum
+    elif col in agency_cols:
+        funcs[col] = stats.mode
+
+df_areas = grouped.agg(funcs)
+
+for col in agency_cols:
+    df_areas[col] = df_areas[col].apply(lambda x: x.mode[0])
+
+# Plot area and poluation served bins
+n_bins = 20
+n_ad_bins = 4
+
+fig, axes = plt.subplots(2, 2, sharey=True, figsize=(12, 12))
+axes[0, 0].hist(df_areas.UZA_Area_SQ_Miles, bins=n_bins)
+axes[0, 0].set_title('UZA Area', fontsize=12)
+axes[0, 0].set_ylabel('Number of Agencies', fontsize=12)
+axes[0, 0].set_xlabel('Square Miles', fontsize=12)
+
+axes[0, 1].hist(df_areas.Service_Area_SQ_Miles, bins=n_bins)
+axes[0, 1].set_title('Service Area', fontsize=12)
+axes[0, 1].set_xlabel('Square Miles', fontsize=12)
+
+axes[1, 0].hist(df_areas.UZA_Population / 1e6, bins=n_bins)
+axes[1, 0].set_ylabel('Number of Agencies', fontsize=12)
+axes[1, 0].set_title('UZA Population', fontsize=12)
+axes[1, 0].set_xlabel('Population (millions)', fontsize=12)
+
+axes[1, 1].hist(df_areas.Service_Area_Population / 1e6,  bins=n_bins)
+axes[1, 1].set_title('Service Area Population', fontsize=12)
+axes[1, 1].set_xlabel('Population (millions)', fontsize=12)
+fig.suptitle('Distribution of Area and Population Sizes Served',
+             fontsize=14,
+             y=.92)
+plt.show()
+
+for col in agency_cols:
+    df_areas[col + '_bin'] = pd.cut(df_areas[col],
+                                    bins=n_ad_bins,
+                                    labels=np.arange(0, n_ad_bins))
+
+for i, col in enumerate(agency_cols):
+    # _, bin_arr = pd.cut(df_areas[col], bins=n_bins, retbins=True)
+    _, bin_arr = pd.qcut(df_areas[col], q=n_ad_bins, retbins=True)
+    legend = defaultdict()
+
+    if i % 2 == 0:
+        s = 'Area'
+    else:
+        s = 'Population'
+
+    for j in range(0, n_ad_bins):
+        if j == 0:
+            legend[j] = (s + ' < ' + str(int(round(bin_arr[j + 1], -2))))
+        elif j != n_ad_bins - 1:
+            legend[j] = (str(int(round(bin_arr[j], -2))) +
+                         ' < ' + s + ' < ' +
+                         str(int(round(bin_arr[j + 1], -2))))
+        else:
+            legend[j] = (s + ' > ' + str(int(round(bin_arr[j], -2))))
+
+    df = df_areas[[*sum_cols, col + '_bin']].\
+                   groupby(col + '_bin').agg('sum').rename(index=legend)
+    df.T.plot(figsize=(12, 12)).legend(loc='best')
+    plt.title(col, fontsize=14)
+    plt.ylabel('Total Annual Rides', fontsize=12)
+    plt.show()
+
+# 2.4 Investigate relationship between changes in total annual ridership and
+# fares, costs and trip length
+
+# Select columns, interpolate missing values
+UPT_cols = ['5_digit_NTD_ID',
+            'Unlinked_Passenger_Trips_FY',
+            'Fares_FY',
+            'Operating_Expenses_FY',
+            'Average_Trip_Length_FY',
+            'Average_Cost_per_Trip_FY',
+            'Average_Fares_per_Trip_FY']
+
+for col in full_data.columns:
+    if col.endswith('UPT'):
+        UPT_cols.append(col)
+
+df_UPT = full_data[UPT_cols]
+
+df_UPT = df_UPT.dropna(thresh=60)
+df_UPT_int = df_UPT[UPT_cols[7:]].interpolate(axis=1,
+                                              limit=None,
+                                              limit_direction='both')
+df_UPT_int[UPT_cols] = df_UPT[UPT_cols]
+
+df_UPT_int['Margin'] = df_UPT_int['Average_Fares_per_Trip_FY'] -\
+                                  df_UPT_int['Average_Cost_per_Trip_FY']
+# Plot the distribution of margins
+df = pd.DataFrame()
+df['pos'] = df_UPT_int['Margin'].sort_values(ascending=False).\
+                                 apply(lambda x: 0 if x < 0 else x)
+df['neg'] = df_UPT_int['Margin'].sort_values(ascending=False).\
+                                 apply(lambda x: 0 if x > 0 else x)
+plt.bar(np.arange(0, df_UPT_int.shape[0], 1), df['pos'], color='g', width=1)
+plt.bar(np.arange(0, df_UPT_int.shape[0], 1), df['neg'], color='r', width=1)
+plt.ylim(-100, 20)
+plt.ylabel('Net Revenue per Trip ($)')
+plt.tick_params(bottom=False, labelbottom=False)
+plt.title('Distribution of net Revenue per Trip')
+plt.show()
+
+df_UPT_by_agency = df_UPT_int.groupby('5_digit_NTD_ID').agg('sum')
+df_UPT_by_agency.drop('Average_Trip_Length_FY', axis=1, inplace=True)
+df_UPT_by_agency['Average_Cost_per_Trip_FY'] =\
+                 df_UPT_by_agency.Operating_Expenses_FY /\
+                 df_UPT_by_agency.Unlinked_Passenger_Trips_FY
+df_UPT_by_agency['Average_Fares_per_Trip_FY'] =\
+                 df_UPT_by_agency.Fares_FY /\
+                 df_UPT_by_agency.Unlinked_Passenger_Trips_FY
+df_UPT_by_agency['Margin'] = df_UPT_by_agency['Average_Fares_per_Trip_FY'] -\
+                                  df_UPT_by_agency['Average_Cost_per_Trip_FY']
+df_UPT_by_agency.head()
+
+df = pd.DataFrame()
+df['pos'] = df_UPT_by_agency['Margin'].sort_values(ascending=False).\
+                                 apply(lambda x: 0 if x < 0 else x)
+df['neg'] = df_UPT_by_agency['Margin'].sort_values(ascending=False).\
+                                 apply(lambda x: 0 if x > 0 else x)
+plt.bar(np.arange(0, df_UPT_by_agency.shape[0], 1), df['pos'],
+        color='g', width=1)
+plt.bar(np.arange(0, df_UPT_by_agency.shape[0], 1), df['neg'],
+        color='r', width=1)
+plt.ylim(-100, 20)
+plt.ylabel('Net Revenue per Trip ($)')
+plt.tick_params(bottom=False, labelbottom=False)
+plt.title('Distribution of net Revenue per Trip by Agency')
+plt.show()
+
+
+
+# 2.5 Investigate the target variable - increase or decrease in ridership
 
 # Objective 3: Create a US map of transit agenceis
 
