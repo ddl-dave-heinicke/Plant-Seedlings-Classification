354496c0f96a2fb4eb8496c0748c77f2a5960b74
Dave <31716582+dheinicke1@users.noreply.github.com>
2018-10-13 12:34:44 -0600

    models added

--- Transit/files/model.py
+++ Transit/files/model.py
@@ -3,12 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from scipy import stats
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import roc_auc_score, precision_recall_curve, roc_curve
+
 
 import seaborn as sns
 plt.style.use('seaborn')
 
-# DATA_PATH = 'C:\\Users\\Dave\\Documents\\Python Scripts\\Transit\\'
-DATA_PATH = 'C:\\Users\\dheinicke\\Google Drive\\Data Science Training\\Python Scripts\\Transit\\'
+DATA_PATH = 'C:\\Users\\Dave\\Documents\\Python Scripts\\Transit\\'
+# DATA_PATH = 'C:\\Users\\dheinicke\\Google Drive\\Data Science Training\\Python Scripts\\Transit\\'
 
 # ROC curve
 def plot_roc_curve(test_y, preds_proba):
@@ -113,8 +118,9 @@ df_UPT['target'].describe()
 df_UPT.head()
 
 # ### Create Master Features ####
-master.shape
+
 master = master.fillna(0)
+
 # Encode transit modes
 master = pd.concat([master.drop('Modes', axis=1),
                     pd.get_dummies(master.Modes, prefix='mode_')], axis=1)
@@ -258,19 +264,13 @@ master['cost_per_person'] = master.\
 
 master = master.apply(lambda x: x.fillna(x.mean()), axis=0)
 
-# master.describe()
-# master.info()
-# mode_cols = [col for col in master.columns if 'mode_' in col]
-# mode_cols
-# master['mode__SR'].describe()
+columns_to_scale = [column for column in master.columns
+                           if type(master[column][1]) == np.float64]
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-import lightgbm as lgb
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.metrics import roc_auc_score, precision_recall_curve, roc_curve
+scaler = StandardScaler()
 
+for column in columns_to_scale:
+    master[column] = scaler.fit_transform(master[column].values.reshape(-1,1))
 
 X = master.drop('target', axis=1)
 y = master.target
@@ -279,71 +279,36 @@ train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2,
                                                     random_state=1)
 
 
-rf = RandomForestClassifier()
+# master.describe()
+# master.info()
+# mode_cols = [col for col in master.columns if 'mode_' in col]
+# mode_cols
+# master['mode__SR'].describe()
+#
+#
+#
+# rf = RandomForestClassifier()
+#
+# # Tune a lightgbm model
+#
+# # dir(GridSearchCV)
+#
+#
+#
+# preds.mean()
+# y.describe()
 
-# Tune a lightgbm model
-
-# dir(GridSearchCV)
-
-
-lgb_clf = lgb.LGBMClassifier(n_estimators=1000,
-                             objective='binary',
-                             random_state=42,
-                             eval_metric='roc_auc',
-                             )
-
-# lgb_clf.get_params().keys()
-
-param_grid = {'num_leaves': [5, 10, 15, 20],
-              'max_depth': [4, 5, 6],
-              'learning_rate': [0.01, 0.1],
-              'min_split_gain': [0],
-              'min_child_weight': [3, 4, 5, 6, 7],
-              'colsample_bytree': [1],
-              'reg_alpha': [1, 0, 0.1, 0.01],
-              'reg_lambda': [0],
-              }
-
-lgb_cv = GridSearchCV(lgb_clf,
-                      param_grid=param_grid,
-                      cv=4,
-                      scoring='roc_auc',
-                      verbose=1)
-
-# help(lgb_clf)
-# help(lgb_cv)
-
-
-lgb_cv.fit(train_X, train_y)
-
-lgb_cv.best_score_
-lgb_cv.best_params_
-
-preds = lgb_cv.best_estimator_.predict(test_X)
-preds_proba = lgb_cv.best_estimator_.predict_proba(test_X)
-score = lgb_cv.best_estimator_.score(test_X, test_y)
-
-cm = confusion_matrix(test_y, preds)
-print(cm)
-print(classification_report(test_y, preds))
-print(roc_auc_score(test_y, preds))
-
-plot_roc_curve(test_y, preds_proba)
-
-preds.mean()
-y.describe()
-
-
-# Small data! Logisitc Regression?
+# Logisitc Regression auc_roc = 0.58
 
 from sklearn.linear_model import LogisticRegression
 
-lr = LogisticRegression(max_iter=1000,
-                        verbose=1)
+lr = LogisticRegression(max_iter=10000,
+                        verbose=1,
+                        n_jobs=-1)
 
-params_lr = {'penalty': ['l1', 'l2'],
+params_lr = {'penalty': ['l1'],
              'tol': [0.0001],
-             'C':[3, 4, 5],
+             'C':[0.5, 1, 1.5],
              'solver':['liblinear']}
 
 lr_cv = GridSearchCV(lr,
@@ -360,6 +325,137 @@ preds = lr_cv.best_estimator_.predict(test_X)
 preds_proba = lr_cv.best_estimator_.predict_proba(test_X)
 score = lr_cv.best_estimator_.score(test_X, test_y)
 print(score)
+cm = confusion_matrix(test_y, preds)
+print(cm)
+print(classification_report(test_y, preds))
+print(roc_auc_score(test_y, preds))
+
+plot_roc_curve(test_y, preds_proba)
+
+# Naive Bayes - roc_auc 0.47
+
+from sklearn.naive_bayes import GaussianNB
+
+gnb = GaussianNB()
+gnb.fit(train_X, train_y)
+
+preds_gnb = gnb.predict(test_X)
+preds_proba_gnb = gnb.predict_proba(test_X)
+score_gnb = gnb.score(test_X, test_y)
+print(score_gnb)
+cm_gnb = confusion_matrix(test_y, preds)
+print(cm_gnb)
+print(classification_report(test_y, preds_gnb))
+print(roc_auc_score(test_y, preds_gnb))
+
+plot_roc_curve(test_y, preds_proba_gnb)
+
+# KNN
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_jobs=-1)
+
+params_knn = {'n_neighbors': [2, 3, 4],
+              'algorithm': ['ball_tree', 'kd_tree', 'brute']
+              }
+
+knn_cv = GridSearchCV(knn,
+                      param_grid=params_knn,
+                      scoring='roc_auc',
+                      cv=4)
+
+knn_cv.fit(train_X, train_y)
+
+knn_cv.best_score_
+knn_cv.best_params_
+
+preds_knn = knn_cv.best_estimator_.predict(test_X)
+preds_proba_knn= knn_cv.best_estimator_.predict_proba(test_X)
+score_knn = knn_cv.best_estimator_.score(test_X, test_y)
+print(score_knn)
+cm_knn = confusion_matrix(test_y, preds_knn)
+print(cm_knn)
+print(classification_report(test_y, preds_knn))
+print(roc_auc_score(test_y, preds_knn))
+
+plot_roc_curve(test_y, preds_proba_knn)
+
+# Random Forrest
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier(n_estimators=1000,
+                            oob_score=True,
+                            n_jobs=-1,
+                            random_state=42,
+                            )
+
+params_rf = {'criterion': ['gini', 'entropy'],
+             'max_features': [0.1, 0.2, 0.3, 0.4, 0.5],
+             'max_depth': [6, 7, 8],
+             'min_samples_split': [2, 3],
+             'min_samples_leaf': [2, 3]
+             }
+
+rf_cv = GridSearchCV(rf,
+                     param_grid=params_rf,
+                     scoring='roc_auc',
+                     cv=4)
+
+rf_cv.fit(train_X, train_y)
+
+rf_cv.best_score_
+rf_cv.best_params_
+
+preds_rf = rf_cv.best_estimator_.predict(test_X)
+preds_proba_rf= rf_cv.best_estimator_.predict_proba(test_X)
+score_rf = rf_cv.best_estimator_.score(test_X, test_y)
+print(score_rf)
+cm_rf = confusion_matrix(test_y, preds_rf)
+print(cm_rf)
+print(classification_report(test_y, preds_rf))
+print(roc_auc_score(test_y, preds_rf))
+
+plot_roc_curve(test_y, preds_proba_rf)
+
+# LightGBM
+import lightgbm as lgb
+
+lgb_clf = lgb.LGBMClassifier(n_estimators=1000,
+                             objective='binary',
+                             random_state=42,
+                             eval_metric='roc_auc',
+                             n_jobs=-1
+                             )
+
+# lgb_clf.get_params().keys()
+
+param_grid = {'boosting_type': ['gbdt', 'dart', 'rf'],
+              'num_leaves': [5, 10, 20, 30, 50, 70, 100],
+              'max_depth': [6, 7, 8, -1],
+              'learning_rate': [0.01],
+              'min_split_gain': [0],
+              'min_child_samples': [2, 3],
+              'colsample_bytree': [1],
+              'reg_alpha': [0.1, 0, 1],
+              'reg_lambda': [0.1, 0, 1],
+              }
+
+lgb_cv = GridSearchCV(lgb_clf,
+                      param_grid=param_grid,
+                      cv=4,
+                      scoring='roc_auc',
+                      verbose=1)
+
+lgb_cv.fit(train_X, train_y)
+
+lgb_cv.best_score_
+lgb_cv.best_params_
+
+preds = lgb_cv.best_estimator_.predict(test_X)
+preds_proba = lgb_cv.best_estimator_.predict_proba(test_X)
+score = lgb_cv.best_estimator_.score(test_X, test_y)
+
 cm = confusion_matrix(test_y, preds)
 print(cm)
 print(classification_report(test_y, preds))
