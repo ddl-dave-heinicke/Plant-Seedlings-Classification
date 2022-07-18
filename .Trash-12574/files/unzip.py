ee23ee7c5f70213de6256fab3b0b57cabdb88e9b
Dave Heinicke <dave.heinicke@dominodatalab.com>
2022-07-18 10:54:41 -0700

    Unzip Domino Dataset script
    Pushed from Domino: https://test.se-team-sandbox.domino.tech/workspace/dave-heinicke/Plant-Seedlings-Classification?executionId=62d1e1a10c332f63175de0e6

---
+++ .Trash-12574/files/unzip.py
@@ -0,0 +1,37 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1e720dfa",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import zipfile\n",
+    "with zipfile.ZipFile(/mnt/data/Plant-Seedlings-Classification/plant-seedlings-classification.zip, 'r') as zip_ref:\n",
+    "    zip_ref.extractall(/mnt/data/Plant-Seedlings-Classification/plant-seedlings-classification)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
